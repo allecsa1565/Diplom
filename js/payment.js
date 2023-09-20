013b1293bcda30ff0086eabc9846057058912737
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-09-20 22:51:28 +0300

    Update payment.js
    корректировка

--- js/payment.js
+++ js/payment.js
@@ -1,32 +1,69 @@
-const selectSeanse = JSON.parse(localStorage.getItem("buyingMeta"));
+"use strict"
 
-let places = "";
-let price = 0;
+const metaDataBuying = JSON.parse(localStorage.getItem(`buyingMeta`));
+let ticketWrapper;
 
-for (const {
-		row, place, type
-	}
-	of selectSeanse.salesPlaces) {
-	if (places !== "") {
-		places += ", ";
-	}
-	places += `${row}/${place}`;
-	price += type === "standart" ? Number(selectSeanse.priceStandart) : Number(selectSeanse.priceVip);
+window.addEventListener(`load`, () => {
+  ticketWrapper = document.querySelector(`.ticket__info-wrapper`);
+  // Вывести всю информацию по билетам
+  showTicket();
+})
+
+// Вывести всю информацию по билетам
+function showTicket() {
+  // Изменить информацию по брони
+  changeInfoBuying();
+  // Кнопка бронирования
+  btnBuying();
 }
 
-document.querySelector(".ticket__title").innerHTML = selectSeanse.filmName; 
-document.querySelector(".ticket__chairs").innerHTML = places; 
-document.querySelector(".ticket__hall").innerHTML = selectSeanse.hallName;
-document.querySelector(".ticket__cost").innerHTML = price; 
+// Изменить информацию по брони
+function changeInfoBuying() {
+  ticketWrapper.querySelector(`.ticket__title`).textContent = metaDataBuying.movieTitle;
+  ticketWrapper.querySelector(`.ticket__chairs`).textContent = metaDataBuying.rowAndSeat;
+  ticketWrapper.querySelector(`.ticket__hall`).textContent = metaDataBuying.hallName;
+  ticketWrapper.querySelector(`.ticket__start`).textContent = metaDataBuying.seanceTime;
+  ticketWrapper.querySelector(`.ticket__cost`).textContent = metaDataBuying.price;
+}
 
-const newHallConfig = selectSeanse.hallConfig.replace(/selected/g, "taken");
-document.querySelector(".acceptin-button").addEventListener("click", (event) => {
-    event.preventDefault();
-    fetch("https://jscp-diplom.netoserver.ru/", {
-        method: "POST",
-        headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded'
-        },
-        body: `event=sale_add&timestamp=${selectSeanse.seanceTimeStamp}&hallId=${selectSeanse.hallId}&seanceId=${selectSeanse.seanceId}&hallConfiguration=${newHallConfig}`,
-    });
-});
+// Кнопка бронирования
+function btnBuying() {
+  ticketWrapper.querySelector(`.acceptin-button`).addEventListener(`click`, () => {
+    // Отправка забронированных билетов на сервер
+    sendBooking();
+    // Создание мета данных выбранных билетов для веб-хранилища
+    creatMetaData();
+    // Удалить мета данные о сеансе и билетах
+    deleteMetaData();
+    // Переход к странице с QR кодом
+    location.href = 'ticket.html';
+  })
+}
+
+// Отправка забронированных билетов на сервер
+function sendBooking() {
+  const xhr = {
+    method: 'POST',
+    url: `https://jscp-diplom.netoserver.ru/`,
+    setRequestHeader: {header: 'Content-type', headerValue:'application/x-www-form-urlencoded'},
+    event: `event=sale_add&timestamp=${metaDataBuying.seanceTimeStamp}&hallId=${metaDataBuying.hallId}&seanceId=${metaDataBuying.seanceId}&hallConfiguration=${metaDataBuying.hallConfig}`,
+  }
+  createRequest(xhr);
+}
+
+// Создание мета данных выбранных билетов для веб-хранилища
+function creatMetaData() {
+  const transMetaDataBuying = metaDataBuying;
+  transMetaDataBuying.qrMeta = `Фильм: ${metaDataBuying.movieTitle};
+  Зал: ${metaDataBuying.hallName};
+  Ряд/Место: ${metaDataBuying.rowAndSeat};
+  Начало сеанса: ${metaDataBuying.seanceTime};
+  Стоимость: ${metaDataBuying.price}`;
+  localStorage.removeItem(`buyingMeta`);
+  localStorage.setItem(`buyingMeta`, JSON.stringify(transMetaDataBuying));
+}
+
+// Удалить мета данные о сеансе и билетах
+function deleteMetaData() {
+  localStorage.removeItem(`seanceMeta`);
+}
