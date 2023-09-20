382e0f806db142c19d842eefbc403a5b6c0a64c2
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-09-21 00:14:41 +0300

    Update createRequest.js
    корректировка

--- js/createRequest.js
+++ js/createRequest.js
@@ -1,28 +1,79 @@
-const createRequest = (options = {}) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(options.method || "POST", options.url, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(options.params);
-    console.log("ПОШЕЛ ЗАПРОС К СЕРВЕРУ!");
+"use strict"
 
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                try {
-                    const response = JSON.parse(xhr.responseText);
-                    for (let key in response) {
-                        if (response[key].err) {
-                            alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
-                            return;
-                        }
-                    }
-                    options.callback(response);
-                } catch (err) {
-                    alert(`Ошибка ответа: ${err.message}`);
-                }
-            } else {
-                alert(`Ошибка запроса: ${xhr.status} ${xhr.statusText}`);
-            }
-        }
-    };
-};
+function createRequest(request) {
+  const xhr = new XMLHttpRequest;
+  xhr.open(request.method, request.url);
+  if (request.responseType) {
+    xhr.responseType = request.responseType;
+  }
+  xhr.setRequestHeader(request.setRequestHeader.header, request.setRequestHeader.headerValue);
+  // try {
+  //   xhr.send(request.event);
+  // } catch (error) {
+  //   const err = new Error('Request Error');
+  //   return err
+  // }
+  xhr.send(request.event);
+  return xhr
+}
+
+// const createRequest = (options = {}) => {
+//   const f = function () {},
+//       {
+//           method = 'GET',
+//           headers = {},
+//           success = f,
+//           error = f,
+//           callback = f,
+//           responseType,
+//           async = true,
+//           data = {}
+//       } = options,
+//       xhr = new XMLHttpRequest;
+
+//   let { url } = options;
+
+//   let requestData;
+//   if (responseType) {
+//       xhr.responseType = responseType;
+//   }
+//   xhr.onload = function() {
+//       success.call( this, xhr.response );
+//       callback.call( this, null, xhr.response );
+//   };
+//   xhr.onerror = function() {
+//       const err = new Error( 'Request Error' );
+//       error.call( this, err );
+//       callback.call( this, err );
+//   };
+
+//   xhr.withCredentials = true;
+
+//   if ( method === 'GET' ) {
+//       const urlParams = Object.entries( data )
+//           .map(([ key, value ]) => `${key}=${value}` )
+//           .join( '&' );
+//       if ( urlParams ) {
+//           url += '?' + urlParams;
+//       }
+//   }
+//   else {
+//       requestData = Object.entries( data )
+//           .reduce(( target, [ key, value ]) => {
+//               target.append( key, value );
+//               return target;
+//           }, new FormData );
+//       // requestData = data;
+//   }
+//   try {
+//       xhr.open( method, url, async );
+//       xhr.send( requestData );
+//   }
+//   catch ( err ) {
+//       error.call( this, err );
+//       callback.call( this, err );
+//       return xhr;
+//   }
+
+//   return xhr;
+// };
