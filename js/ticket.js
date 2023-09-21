0b336f2a97bf3362d0f020231bd135b74dded0a1
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-09-21 13:06:54 +0300

    Update ticket.js
    корректировка

--- js/ticket.js
+++ js/ticket.js
@@ -1,39 +1,32 @@
-const selectSeanse = JSON.parse(localStorage.getItem('selectSeanse'));
+const selectSeance = JSON.parse(localStorage.getItem("selectSeance"));
 
-let places = "";
-let price = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  let places = [];
+  selectSeance.salesPlaces.forEach((elem) => {
+    places.push(" " + elem.row + "/" + elem.place);
+  });
 
-for (const {
-		row, place, type
-	}
-	of selectSeanse.salesPlaces) {
-	if (places !== "") {
-		places += ", ";
-	}
-	places += `${row}/${place}`;
-	price += type === "standart" ? Number(selectSeanse.priceStandart) : Number(selectSeanse.priceVip);
-}
+  document.querySelector(".ticket__title").innerHTML = selectSeance.filmName;
+  document.querySelector(".ticket__hall").innerHTML = selectSeance.hallName;
+  document.querySelector(".ticket__start").innerHTML = selectSeance.seanceTime;
+  document.querySelector(".ticket__chairs").innerHTML = places;
 
-document.querySelector(".ticket__title").innerHTML = selectSeanse.filmName;  
-document.querySelector(".ticket__chairs").innerHTML = places; 
-document.querySelector(".ticket__hall").innerHTML = selectSeanse.hallName;  
-document.querySelector(".ticket__start").innerHTML = selectSeanse.seanceTime;  
-document.querySelector(".ticket__cost").innerHTML = price;  
+  const date = new Date(Number(selectSeance.seanceTimeStamp * 1000));
+  const strDate = date.toLocaleDateString("ru-RU", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+  //Формируем данные для QR кода
+  let textQR = `    
+  Фильм: ${selectSeance.filmName}
+  Зал: ${selectSeance.hallName}
+  Ряд/Место ${places}
+  Дата: ${strDate}
+  Начало сеанса: ${selectSeance.seanceTime}
+  Билет действителен строго на свой сеанс`;
 
-const newHallConfig = selectSeanse.hallConfig.replace(/selected/g, "taken");
+  const qr = QRCreator(textQR, { image: "SVG" }); //Создаем QR
 
-console.log(selectSeanse.seanceTimeStamp);
-console.log(selectSeanse.hallId);
-console.log(selectSeanse.seanceId);
-console.log(newHallConfig);
-
-document.querySelector(".acceptin-button").addEventListener("click", (event) => {
-	event.preventDefault();
-	fetch("https://jscp-diplom.netoserver.ru/", {
-		method: "POST",
-		headers: {
-			'Content-Type' : 'application/x-www-form-urlencoded'
-		},
-		body: `event=sale_add&timestamp=${selectSeanse.seanceTimeStamp}&hallId=${selectSeanse.hallId}&seanceId=${selectSeanse.seanceId}&hallConfiguration=${newHallConfig}`,
-	});
+  document.querySelector(".ticket__info-qr").append(qr.result); //Добавляем готовый QR на страницу
 });
