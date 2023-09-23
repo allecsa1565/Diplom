c7ae90e06c92d34245b7ae7a4e4fe8a2b00b7fe9
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-09-23 08:30:53 +0300

    Update ticket.js

--- js/ticket.js
+++ js/ticket.js
@@ -37,7 +37,6 @@ console.log(selectSeanse);
   const dateStr = date.toLocaleDateString("ru-RU", {
 		day: "2-digit",
 		month: "2-digit",
-		year: "numeric"
 	});
 
   let textQR = `Фильм: ${selectSeanse.filmName} Зал: ${
